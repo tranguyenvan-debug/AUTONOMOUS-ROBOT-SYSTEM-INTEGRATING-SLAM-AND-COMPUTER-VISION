20fcbd5762c839ed43b0972576e08a3b589e558d
Văn Trà <tra.nguyenvan@hcmut.edu.vn>
2026-07-15 13:05:51 +0700

    Update send_ip.sh

--- send_ip.sh
+++ send_ip.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 sleep 15
 IP=$(hostname -I | awk '{print $1}')
-TOKEN="8795253957:AAFvQ2_2RuH5EGLKho7PwAC8Iuj9JJFuo4c"
+TOKEN="YOUR_TELEGRAM_BOT_TOKEN_HERE"
 CHAT_ID="7056257229"
 curl -s -X POST "https://api.telegram.org/bot${TOKEN}/sendMessage" \
   -d chat_id="${CHAT_ID}" \
